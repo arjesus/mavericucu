df3bd25841ff0478c6b92837b1937a5e59d5ede2
Ismael Jaouhar Babiano <ijaouhar@everis.com>
2020-04-01 20:50:28 +0200

    working

--- src/components/dynamic-form/dynamic-form.js
+++ src/components/dynamic-form/dynamic-form.js
@@ -6,44 +6,62 @@ const feelings = ["Ansiedad", "Tristeza", "Depresion", "Agobio", "Miedo"];
 const intensity = ["Ligero", "Esta incrementandose", "Es intenso", "Necesito ayuda urgente"];
 
 class DynamicForm extends React.Component {
-  state = {};
+  state = {
+    formStep: 1
+  };
+
+  renderNextStep = () => {
+    const { formStep } = this.state;
+    console.log(formStep)
+    this.setState({
+      formStep: formStep + 1
+    });
+  }
 
   render() {
+    const { formStep } = this.state;
+    console.log(formStep, '22222222222222222222222222222')
     return (
       <React.Fragment>
         <div className="form-container">
-          {/* <div className="feel-container">
-            <h3>¿Como te sientes?</h3>
-            <div className="btn-container">
-              {feelings.map(feeling => {
-                return (
-                  <a className="feel-btn" href="#" key={indexedDB.toString()}>
-                    <span>{feeling}</span>
-                  </a>
-                )
-              })}
+          {formStep === 1 && (
+            <div className="feel-container">
+              <h3>¿Como te sientes?</h3>
+              <div className="btn-container">
+                {feelings.map(feeling => {
+                  return (
+                    <a className="feel-btn" href="#" key={indexedDB.toString()} onClick={() => this.renderNextStep()}>
+                      <span>{feeling}</span>
+                    </a>
+                  )
+                })}
+              </div>
             </div>
-          </div> */}
-          {/* <div className="feel-container">
-            <h3>¿Como de intensos son tus sentimientos?</h3>
-            <div className="btn-container">
-              {intensity.map(intens => {
-                return (
-                  <a className="feel-btn" href="#" key={indexedDB.toString()}>
-                    <span>{intens}</span>
-                  </a>
-                )
-              })}
+          )}
+          {formStep === 2 && (
+            <div className="feel-container">
+              <h3>¿Como de intensos son tus sentimientos?</h3>
+              <div className="btn-container">
+                {intensity.map(intens => {
+                  return (
+                    <a className="feel-btn" href="#" key={indexedDB.toString()} onClick={() => this.renderNextStep()}>
+                      <span>{intens}</span>
+                    </a>
+                  )
+                })}
+              </div>
             </div>
-          </div> */}
-          <div className="feel-container">
-            <h3>
-              Dejanos tus datos y nos pondremos en contacto contigo para asignarte un profesional
+          )}
+          {formStep === 3 && (
+            <div className="feel-container">
+              <h3>
+                Dejanos tus datos y nos pondremos en contacto contigo para asignarte un profesional
             </h3>
-            <div className="form-integrated">
-              <ThemeContext.Consumer>{theme => <Contact theme={theme} />}</ThemeContext.Consumer>
+              <div className="form-integrated">
+                <ThemeContext.Consumer>{theme => <Contact theme={theme} />}</ThemeContext.Consumer>
+              </div>
             </div>
-          </div>
+          )}
         </div>
         {/* --- STYLES --- */}
         <style jsx>{`
