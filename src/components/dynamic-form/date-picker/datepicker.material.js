39d4b33bf652b18bdc2889147567b7a655b44ff8
Ismael Jaouhar Babiano <ijaouhar@everis.com>
2020-04-10 18:26:42 +0200

    wip

--- src/components/dynamic-form/date-picker/datepicker.material.js
+++ src/components/dynamic-form/date-picker/datepicker.material.js
@@ -1,29 +1,23 @@
 import React from "react";
 import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import DateFnsUtils from "@date-io/date-fns";
 import userHandleDate from "../handleDateChange";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#4e3b80"
+    }
+  }
+});
 
 export default function DatePickerMaterialComponent() {
   const id = "materialDatePicker";
   const dateHandler = userHandleDate(new Date(), id);
   return (
-    <React.Fragment>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker {...dateHandler} orientation="landscape" variant="static" />
-        <style jsx>{`
-          .MuiToolbar-root {
-            background-color: red;
-          }
-          .picker__weekday-display {
-            background-color: red;
-          }
-          .picker__day--selected,
-          .picker__day--selected:hover,
-          .picker--focused .picker__day--selected {
-            background-color: blue;
-          }
-        `}</style>
-      </MuiPickersUtilsProvider>
-    </React.Fragment>
+    <MuiPickersUtilsProvider utils={DateFnsUtils} theme={theme}>
+      <DatePicker theme={theme} {...dateHandler} orientation="landscape" variant="static" />
+    </MuiPickersUtilsProvider>
   );
 }
