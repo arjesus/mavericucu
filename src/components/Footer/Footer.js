03f453978dde1411e79b16f9ad9b36b935ae0fb6
Ismael Jaouhar Babiano <ijaouhar@everis.com>
2020-04-24 21:10:59 +0200

    wip

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,48 +1,40 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { Link } from 'gatsby';
+import React from 'react';
+import PropTypes from 'prop-types';
+import theme from '../../theme/theme.yaml';
 
 const Footer = props => {
-  const { html, theme } = props;
-
   return (
     <React.Fragment>
-      <footer className="footer" dangerouslySetInnerHTML={{ __html: html }} />
-
+      <footer className="footer">
+        <ul>
+          <Link to="/privacy" className="footer-link">
+            Privacidad
+          </Link>
+          <Link to="/cookies" className="footer-link">
+            Cookies
+          </Link>
+          <Link to="/terms" className="footer-link">
+            Términos
+          </Link>
+        </ul>
+      </footer>
       {/* --- STYLES --- */}
       <style jsx>{`
         .footer {
-          background: ${theme.color.neutral.white};
+          align-items: center;
+          display: flex;
+          flex-direction: 'column';
+          background: ${theme.color.principals.darkerPurpleText};
           padding: ${theme.space.inset.default};
-          padding-top: 0;
-          padding-bottom: 120px;
+          top: 0;
+          width: 100%;
+          height: 15vh;
+          justify-content: center;
 
-          :global(ul) {
-            list-style: none;
-            text-align: center;
-            padding: 0;
-
-            :global(li) {
-              color: ${theme.color.neutral.gray.g};
-              font-size: ${theme.font.size.xxs};
-              padding: ${theme.space.xxs} ${theme.space.s};
-              position: relative;
-              display: inline-block;
-
-              &::after {
-                content: "•";
-                position: absolute;
-                right: ${`calc(${theme.space.xs} * -1)`};
-              }
-              &:last-child::after {
-                content: "";
-              }
-            }
-          }
-        }
-
-        @from-width desktop {
-          .footer {
-            padding: 0 1em 1.5em;
+          & :global(a.footer-link) {
+            color: ${theme.color.principals.white};
+            margin: 0 20px;
           }
         }
       `}</style>
