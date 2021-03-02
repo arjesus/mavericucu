32607e535313525d11181ff885ce3a858abbed93
Ismael <ismael.jaouhar@gmail.com>
2021-03-02 20:23:36 +0100

    wip

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteTitle: 'Eva Minerva', // <title>
+  shortSiteTitle: 'Eva Minerva', // <title> ending for posts and pages
   siteDescription:
     'Eva Minerva, plataforma de psicologia que pone en contacto a profesionales y gente interesada en cuidar su salid.',
   siteUrl: 'https://evaminerca.com',
@@ -21,13 +22,6 @@ module.exports = {
   manifestBackgroundColor: 'white',
   manifestThemeColor: '#666',
   manifestDisplay: 'standalone',
-
-  // gravatar
-  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
-  // Replace your email adress with md5-code.
-  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
-  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  // gravatarImgMd5: "",
 
   // social
   authorSocialLinks: [
