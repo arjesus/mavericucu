e83fa193bc3d44c0e171f9b58f7dc8dd25c05fec
Ismael <ismael.jaouhar@gmail.com>
2021-03-22 18:26:19 +0100

    changing cookies and change post url

--- src/components/cookiesComponent/cookies.component.js
+++ src/components/cookiesComponent/cookies.component.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Grid, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
 import theme from '../../theme/theme.yaml';
-import cookies from '../../images/jpg/Las galletas.svg';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import Switch from '@material-ui/core/Switch';
 import { FaFileExcel } from 'react-icons/fa';
 
@@ -17,11 +19,13 @@ const useStyles = makeStyles({
     paddingButton: '15px'
   },
   cookiesBlockBox: {
-    backgroundColor: theme.color.principals.darkerPurpleText,
-    width: '60%',
+    backgroundColor: theme.color.principals.white,
+    width: '100%',
     borderRadius: '2rem',
     marginBottom: '20px',
-    padding: '20px 20px'
+    padding: '20px 20px',
+    border: '0.5px solid black',
+    opacity: '0.8'
   },
   cookiesText: {},
   cookies: {
@@ -34,18 +38,19 @@ const useStyles = makeStyles({
   },
   button: {
     backgroundColor: theme.color.principals.orange,
-    padding: '5px 0px',
-    color: 'white',
+    padding: '5px 7px',
+    color: theme.color.principals.white,
     width: '150px',
     fontWeight: '600',
     textAlign: 'center',
     fontSize: '1.2rem',
-    borderRadius: '0.5rem'
+    borderRadius: '0.5rem',
+    height: '35px'
   },
   CookiesParagraph: {
-    fontSize: '1.2rem',
-    color: theme.color.principals.white,
-    marginBottom: '2rem'
+    fontSize: '0.8rem',
+    color: theme.color.principals.darkerPurpleText,
+    marginBottom: '0.2rem'
   },
   img: {
     width: '150px',
@@ -57,6 +62,21 @@ const useStyles = makeStyles({
     flexDirection: 'row',
     justifyContent: 'flex-end'
   },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  paper: {
+    backgroundColor: theme.color.principals.white,
+    maxWidth: '95vw',
+    padding: '15px',
+    borderRadius: '0.5rem'
+  },
+  configurationButton: {
+    backgroundColor: 'none',
+    border: 'none'
+  },
   '@media (max-width: 700px)': {
     cookiesBlockBox: {
       width: '100%',
@@ -65,7 +85,7 @@ const useStyles = makeStyles({
       padding: '0px 20px 10px 20px'
     },
     CookiesParagraph: {
-      fontSize: '0.7rem',
+      fontSize: '0.8rem',
       marginBottom: '1rem'
     },
     img: {
@@ -82,29 +102,75 @@ const handleClick = () => {
 
 const CookiesBlock = props => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Box mt={2} className={classes.cookiesBlock}>
       <Grid container className={classes.cookiesBlockBox}>
-        <Grid item md={3} sm={12} className={classes.cookies}>
-          <img className={classes.img} src={cookies} alt="Cookies" />
+        <Grid item md={11} sm={8}>
+          <Grid item md={12} sm={12} className={classes.textCookies}>
+            <p className={classes.CookiesParagraph}>
+              Gracias por visitar Eva Minerva. Como la mayor parte de las webs utilizamos cookies
+              para mejorar la experiencia del usuario. Si continuas navegando entendemos que adaptes
+              los términos y condiciones de uso.{' '}
+              {
+                <button type="button" className={classes.configurationButton} onClick={handleOpen}>
+                  Configurar cookies
+                </button>
+              }{' '}
+              <a href="/cookies">Nuestra politica de cookies</a>
+            </p>
+          </Grid>
         </Grid>
-        <Grid item md={9} sm={12} className={classes.textCookies}>
-          <p className={classes.CookiesParagraph}>
-            Gracias por visitar Eva Minerva. Como la mayor parte de las webs utilizamos cookies para
-            mejorar la experiencia del usuario. Si continuas navegando entendemos que adaptes los
-            términos y condiciones de uso.
-          </p>
-        </Grid>
-        <Grid item md={9} sm={12} className={classes.switchCookies}>
-          <p className={classes.CookiesParagraph}>Cookies de terceros para la gestión de pagos</p>
-          <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
-        </Grid>
-        <Grid item md={12} sm={12} className={classes.cookiesBottom}>
+        <Grid item md={1} sm={4} className={classes.cookiesBottom}>
           <button className={classes.button} onClick={handleClick}>
             Entendido
           </button>
         </Grid>
       </Grid>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+      >
+        <Fade in={open}>
+          <Grid container className={classes.paper}>
+            <Grid item md={9} sm={12} className={classes.switchCookies}>
+              <p className={classes.CookiesParagraph}>
+                Cookies de terceros para la gestión de pagos
+              </p>
+              <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
+            </Grid>
+            <Grid item md={9} sm={12} className={classes.switchCookies}>
+              <p className={classes.CookiesParagraph}>Cookies de Analytics</p>
+              <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
+            </Grid>
+            <Grid item md={9} sm={12} className={classes.switchCookies}>
+              <p className={classes.CookiesParagraph}>Cookies de Facebook ads</p>
+              <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
+            </Grid>
+            <Grid item md={12} sm={12} className={classes.cookiesBottom}>
+              <button className={classes.button} onClick={handleClose}>
+                Guardar
+              </button>
+            </Grid>
+          </Grid>
+        </Fade>
+      </Modal>
     </Box>
   );
 };
--- src/services/form.js
+++ src/services/form.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://eva-minerva.herokuapp.com'
-  baseURL: 'http://localhost:3000/'
+  baseURL: 'https://eva-minerva.herokuapp.com'
 
   //withCredentials: true
 });
